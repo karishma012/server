e48275471dc25bd11dac43cc982f9cc9002511df
karishma012 <115379019+karishma012@users.noreply.github.com>
2023-05-28 13:19:13 +0530

    till 51

--- routes/auth.js
+++ routes/auth.js
@@ -4,11 +4,11 @@ const User = require('../Models/User');
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-
+const fetchuser = require('../middleware/fetchuser')
 const router = express.Router();
 const JWT_SECRET = 'iamagoodgirl$hahah';
 
-//create a user using POST using "/api/auth/createuser"
+//ROUTE 1 : create a user using POST using "/api/auth/createuser"
 //ye sab mene express validator se uthaya
 
 router.post('/createuser', [
@@ -31,10 +31,10 @@ router.post('/createuser', [
         if (user) {
             return res.status(400).json({ error: "sorry a user with same emailId already exists" })
         }
-        const salt = await bcrypt.genSalt(10);
-
-        secPass = await bcrypt.hash(req.body.password, salt);
-        //isse basically jo database me format dekhenge na hum db ka vhi yhi pe dikh jayega or ek naya user create hojayega
+        const salt = await bcrypt.genSalt(10); //salt generate kia
+            //yaha mera JWT ka role aata hai
+        secPass = await bcrypt.hash(req.body.password, salt); //total password = password + salt
+        //ab sab sahi raha toh ek naya user create hojayega
         user = await User.create({
             name: req.body.name,
             password: secPass,
@@ -46,6 +46,7 @@ router.post('/createuser', [
         //     res.json({error:'please enter a unique value for email', message: err.message})
         // })
         //m user ki id bejungi token ke through...id bejke sb sambhal jayega
+        //basically yaha web token ki help se verifivation ho raha hai
         const data = {
             user: {
                 id: user.id
@@ -62,7 +63,7 @@ router.post('/createuser', [
     }
 
 })
-//now here we r going to create a similar page for login, here only email and password will be required so everything is copied and pasted
+//ROUTE 2 : now here we r going to create a similar page for login, here only email and password will be required so everything is copied and pasted
 
 router.post('/login', [
 
@@ -76,15 +77,18 @@ router.post('/login', [
     }
     const { email, password } = req.body;
     try {
+        //ek baar email ke lie check karo or
         let user = await User.findOne({ email });
         if (!user) {
             return res.status(400).json({ error: "please try to login with correct credentials" });
         }
+        //ek baar password ke lie
         const passwordCompare = await bcrypt.compare(password, user.password);
         //agar vo hamare haspassword ke equal nahi hua
         if (!passwordCompare) {
             return res.status(400).json({ error: "please try to login with correct credentials" });
         }
+        //verification
         const data = {
             user: {
                 id: user.id
@@ -100,6 +104,18 @@ router.post('/login', [
         res.status(500).send("Internal Server error occured");
     }
 
+})
+//ROUTE :3 get user details . here are finding user by id..sab kuch id se hi hoga
+router.post('/getuser',fetchuser, async (req, res) => {
+try {
+    
+    userId = req.user.id;
+    const user = await User.findById(userId).select('-password')
+    res.send(user)
+} catch (error) {
+    console.log(error.message);
+    res.status(500).send("Internal Server error occured");
+}
 })
 
 module.exports = router
